cc64a478dc7014c8ae84c334d7c65a89db864a99
OsvaExe <osvaldo.hernandezj@gmail.com>
2025-10-31 12:15:36 -0600

    osvaldo puto

--- juego.c
+++ juego.c
@@ -4,3 +4,10 @@ int main(int argc, const char *argv[]){
     printf("FELIPE ES EL PATRON Y APLASTA ABUELITAS");
     return 0;
 }
+
+
+
+
+/*if game is not finished pls finish it and conclude it properly pls :(
+    im not gonna help you sir just gonna send it. Pls sir enter your 
+    microsoft account details to proceed. Thank you for your cooperation.*/
